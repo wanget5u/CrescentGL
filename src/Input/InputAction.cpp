95d8c30261f36b144b9ae2160e5832553f7affb8
Maciej Gałgan <maciekgalgan@protonmail.com>
2026-07-03 21:21:54 +0200

    .

--- src/Input/InputAction.cpp
+++ src/Input/InputAction.cpp
@@ -7,15 +7,15 @@
 
 namespace Crescent::Input {
 
-void Action::BindKeyboardKey(Input::KeyCode code) {
+void Action::BindKeyboardKey(KeyCode const code) {
 	m_Bindings.push_back(Binding::FromKey(code));
 }
 
-void Action::BindMouseButton(Input::MouseButton button) {
+void Action::BindMouseButton(MouseButton const button) {
 	m_Bindings.push_back(Binding::FromMouseButton(button));
 }
 
-void Action::BindMouseAxis(Input::MouseAxis axis, f32 scale) {
+void Action::BindMouseAxis(MouseAxis const axis, f32 const scale) {
 	m_Bindings.push_back(Binding::FromMouseAxis(axis, scale));
 }
 
@@ -25,7 +25,7 @@ Action::SubscriptionID Action::Subscribe(Callback callback) {
 	return id;
 }
 
-void Action::Unsubscribe(Action::SubscriptionID id) {
+void Action::Unsubscribe(SubscriptionID const id) {
 	size_t erasedCount = m_Subscribers.erase(id);
 	if (erasedCount == 0) {
 		Log::Warning("[InputAction] Tried to erase non-existing subscription.");
@@ -39,24 +39,24 @@ void Action::OnUpdate(GLFWwindow* window, f32 mouseDeltaX, f32 mouseDeltaY, f32 
 		return;
 	}
 
-	Action::Phase phase;
+	Phase phase{};
 	bool shouldFire = false;
 
 	if (m_IsActive == true && m_WasActive == false) {
-		phase = Action::Phase::Pressed;
+		phase = Phase::Pressed;
 		shouldFire = true;
 	}
 	else if (m_IsActive == true && m_WasActive == true) {
-		phase = Action::Phase::Held;
+		phase = Phase::Held;
 		shouldFire = true;
 	}
 	else if (m_IsActive == false && m_WasActive == true) {
-		phase = Action::Phase::Released;
+		phase = Phase::Released;
 		shouldFire = true;
 	}
 
 	if (shouldFire == true) {
-		Action::Event event(m_Name, phase, m_AnalogValue);
+		Event event(m_Name, phase, m_AnalogValue);
 		for (auto const& [id, callback] : m_Subscribers) {
 			callback(event);
 		}
@@ -64,7 +64,7 @@ void Action::OnUpdate(GLFWwindow* window, f32 mouseDeltaX, f32 mouseDeltaY, f32 
 	m_WasActive = m_IsActive;
 }
 
-bool Action::EvaluateBindings(GLFWwindow* window, f32 mouseDeltaX, f32 mouseDeltaY, f32 scrollDelta) {
+bool Action::EvaluateBindings(GLFWwindow* window, f32 const mouseDeltaX, f32 const mouseDeltaY, f32 const scrollDelta) {
 	for (Binding const& binding : m_Bindings) {
 		bool matched = false;
 		switch (binding.type) {
